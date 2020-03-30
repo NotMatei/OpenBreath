b210afe75cd717b3e1cb3dbebee97d1e6b9e05e4
TheLastBilly <jtmonegro@gmail.com>
2020-03-30 01:19:29 -0400

    added speed test

--- code/openbreath/openbreath.ino
+++ code/openbreath/openbreath.ino
@@ -1,12 +1,24 @@
 #include "include/esc.h"
 
-ESC esc(9);
+ESC esc(D4);
 
 void setup()
 {
-    esc.Init();
-    esc.SetSpeed(50);
+    Serial.begin( 9600 );
 }
 
 void loop()
-{}
+{
+    while( !Serial.available() );
+    esc.Init();
+    Serial.println("\nESC speed test");
+    char input;
+    for( int i = 60; i < 180; i += 2 )
+    {
+        Serial.print( "Speed will be set to " );
+        Serial.println( i );
+        esc.SetRawSpeed(i);
+        Serial.println( "Changing speed in 5 seconds" );
+        delay( 5000 );
+    }
+}
