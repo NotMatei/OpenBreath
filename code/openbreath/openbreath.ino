a2ffc8fa3f1dfd46e00df492d3e3d6833826029f
TheLastBilly <jtmonegro@gmail.com>
2020-08-16 13:11:22 -0400

    here you go

--- code/openbreath/openbreath.ino
+++ code/openbreath/openbreath.ino
@@ -1,74 +1,27 @@
-#include "include/terminal.h"
-#include "include/breathing.h"
+#include "esc.h"
 
 #define ESC_PIN 9
+#define BUTTON_PIN 8
+#define MOTOR_SPEED 20
 
 ESC esc(ESC_PIN);
-Breathing::Engine engine(esc);
-
-Breathing::PatternElement TestPattern[2] = {
-    {
-        .speed  = 5,
-        .time   = 1000
-    },
-    {
-        .speed  = 2,
-        .time   = 1000
-    },
-};
-
-void print( String msg )
-{
-    Serial.print(msg);
-}
-
-void on_return()
-{
-    String input = Terminal::GetBuffer();
-    if( input == "init esc" )
-    {
-        Terminal::printf("Initialzing ESC...\n\r");
-        engine.Init();
-        Terminal::printf("Done initializing ESC\n\r");
-    }
-    else if( input == "test speed" )
-    {
-        Terminal::printf("Running test pattern...\n\r");
-        engine.RunPattern( TestPattern, 2 );
-        engine.GetESC().Stop();
-        Terminal::printf("Done running test pattern\n\r");
-    }
-    else if( input == "stop motor" )
-    {
-        Terminal::printf("Stopping motor...\n\r");
-        engine.GetESC().Stop();
-        delay(3000);
-        Terminal::printf("Motor stopped\n\r");
-    }
-    else if( input == "run motor" )
-    {
-        Terminal::printf("Setting motor speed to 10%\n\r");
-        engine.GetESC().SetSpeed(10);
-    }
-}
 
 void setup()
-{
-    Serial.begin( 115200 );
-    Terminal::SetReturnCallback( &on_return );
-    Terminal::SetOutputCallback( &print );
-    while(!Serial.available());
-    Terminal::Reset();
-    while (1)
-    {
-        if(Serial.available())
-        {
-            Terminal::AddCharacter(Serial.read());
-        }
-    }
-    
+{ 
+    esc.Init();
+    pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop()
 {
+   static bool should_run = false;
+   bool input = digitalRead(BUTTON_PIN);
+   if(should_run != input)
+   {
+	if(should_run)
+	    esc.SetSpeed(MOTOR_SPEED);
+	else
+	    esc.SetSpeed(0);
+	should_run = input;
+   }
 }
